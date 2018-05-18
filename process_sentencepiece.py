e78ee23738c1f291cf894c152fdfd72ff254cdd0
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-05-17 17:14:13 -0700

    process_sentence_piece

--- process_sentencepiece.py
+++ process_sentencepiece.py
@@ -1,6 +1,17 @@
 # sp.Load("/data/sentence/opennmt_pretrained/sentencepiece.model")
 # python process_sentencepiece.py /data/sentence/opennmt_pretrained/sentencepiece.model data/src-val.txt data/srcp-val.txt
 
+
+# sp.EncodeAsIds("This is a test")
+# sp.DecodePieces(['\xe2\x96\x81This', '\xe2\x96\x81is', '\xe2\x96\x81a', '\xe2\x96\x81', 't', 'est'])
+# sp.NBestEncode("This is a test", 5)
+# sp.DecodeIds([284, 47, 11, 4, 15, 400])
+# sp.GetPieceSize()
+# sp.IdToPiece(2)
+# sp.PieceToId('</s>')
+# len(sp)
+# sp['</s>']
+
 import sys
 import codecs
 import sentencepiece as spm
@@ -13,12 +24,3 @@ with codecs.open(sys.argv[2], "r", "utf-8") as input_file:
     for i, line in enumerate(input_file):
         outfile.write(' '.join(sp.EncodeAsPieces(line) + '\n')
 
-# sp.EncodeAsIds("This is a test")
-# sp.DecodePieces(['\xe2\x96\x81This', '\xe2\x96\x81is', '\xe2\x96\x81a', '\xe2\x96\x81', 't', 'est'])
-# sp.NBestEncode("This is a test", 5)
-# sp.DecodeIds([284, 47, 11, 4, 15, 400])
-# sp.GetPieceSize()
-# sp.IdToPiece(2)
-# sp.PieceToId('</s>')
-# len(sp)
-# sp['</s>']
